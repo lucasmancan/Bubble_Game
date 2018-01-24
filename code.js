d0b735cb162f65866905a56ad88d12aa84005b33
= <=>
2018-01-24 18:21:00 -0200

    Game

--- code.js
+++ code.js
@@ -13,7 +13,7 @@ var bola = 0;
 var tamanho = 0;
 var container = 0;
 var id =0;
-var click = false;
+
 var width=0;
 var height=0;
 var left = 0;
@@ -21,6 +21,8 @@ var top = 0;
 var timeId= 0
 var inicio =0;
 var fim = 0;
+var textoFim = document.getElementById("fimJogo");
+var textoPontuacao = document.getElementById("pontuacao");
 //variáveis do jogo
 
 window.onload = function(){
@@ -28,27 +30,24 @@ window.onload = function(){
   height = innerHeight;
   
 }
-function aparecerBolas(numbolas){
- 
-  for (var i = 0; i < numbolas; i++) {
-    mudarBola(i);
-  }
-}
+
 function start(difficult) {
 
-  document.getElementById('btn').style.display = "none";
+  document.getElementById('config').style.display = "none";
   document.getElementById('logo').style.display = "none";
-  document.getElementById('dificuldade').style.display = "none";
+  
  
   switch (difficult) {
     case "1":
+    document.getElementById("fimJogo").style.display = "none";
+    document.getElementById("pontuacao").style.display = "block";
       numbolas = 6;
       tempoDeJogo = 15;
       tempoDeTroca = 1500;
       score = 0;
       inicio = new Date().getTime();
       document.getElementById("score").innerHTML = score;
-      if(id ==0){
+      if(id< numbolas){
         criarBola(numbolas);
       }else{
         aparecerBolas(numbolas);
@@ -57,13 +56,15 @@ function start(difficult) {
       break;
 
     case "2":
+    document.getElementById("fimJogo").style.display = "none";
+    document.getElementById("pontuacao").style.display = "block";
       numbolas = 4;
       tempoDeJogo = 15;
-      tempoDeTroca = 1500;
+      tempoDeTroca = 1000;
       score = 0;
       document.getElementById("score").innerHTML = score;
       inicio = new Date().getTime();
-      if(id ==0){
+      if(id< numbolas){
         criarBola(numbolas);
       }else{
         aparecerBolas(numbolas);
@@ -72,14 +73,18 @@ function start(difficult) {
       break;
 
     case "3":
+    document.getElementById("fimJogo").style.display = "none";
+    document.getElementById("pontuacao").style.display = "block";
       numbolas = 3;
       tempoDeJogo = 15;
-      tempoDeTroca =  1500;
+      tempoDeTroca =  500;
       score = 0;
       document.getElementById("score").innerHTML = score;
       inicio = new Date().getTime();
-      if(id ==0){
+      if(id< numbolas){
         criarBola(numbolas);
+      }else{
+        aparecerBolas(numbolas);
       }
       timeId = setInterval(aparecerBolas, tempoDeTroca);
      
@@ -89,19 +94,37 @@ function start(difficult) {
       document.innerHTML = "Selecione uma opção";
   }
 }
-
+function aparecerBolas(){
+ 
+  for (var i = 0; i < numbolas; i++) {
+    
+    mudarBola(i);
+  }
+}
 
   function FimJogo(){
-    document.getElementById('btn').style.display = "block";
+   
     document.getElementById('logo').style.display = "block";
-    document.getElementById('dificuldade').style.display = "block";
+    document.getElementById('config').style.display = "block";
+   
     clearInterval(timeId);
     
     for(var i =0; i <numbolas; i++){
       desaparecerForma(i);
 
     }
+    if(score == 0){
+      document.getElementById("fimJogo").innerHTML = "Ops, não foi sua melhor partida né?";
+    }else if(score <= 10){
+      document.getElementById("fimJogo").innerHTML = "Poxa, você conseguiu "+score+" pontos, tenho certeza que consegue mais!";
+    }else if(score > 10 && score <=20){
+      document.getElementById("fimJogo").innerHTML = "Parabéns, você foi muito bem e conseguiu "+score+" pontos!";
+    }else if(score > 20){
+      document.getElementById("fimJogo").innerHTML = "Excelente, você conseguiu "+score+" pontos, já pode se achar o maioral!!";
+    }
     
+    document.getElementById("fimJogo").style.display = "block";
+   document.getElementById("pontuacao").style.display = "none";
   }
 function Bola() {
   bola = document.createElement('div');
@@ -109,6 +132,7 @@ function Bola() {
   bola.setAttribute("id", id);
   // Criar elemento
   bola.setAttribute("onclick", "desaparecerForma("+id+");");
+  
   document.body.appendChild(bola);
   mudarBola(id);
   id++;
@@ -143,7 +167,7 @@ function mudarBola(id) {
   
 }
 function criarBola(numbolas) {
-  for (var i = 0; i < numbolas; i++) {
+  for (var i = id; i < numbolas; i++) {
     Bola(); 
   }
 }
@@ -151,7 +175,12 @@ function criarBola(numbolas) {
 function desaparecerForma(id) {
   
   document.getElementById(id).style.display = "none";
-  score++;
+  if((fim-inicio)/1000 >= tempoDeJogo){
+    
+  }else{
+    score++;
+  }
+  
   document.getElementById("score").innerHTML = score;
   
 }
